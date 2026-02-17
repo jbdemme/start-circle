9b38e787a0f578d87ed34f78d41de5728a4f95f3
Jonathan Demme <j.b.demme@gmail.com>
2026-02-17 04:58:32 +0100

    add sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import * as React from "react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import {
+  Settings2Icon,
+  House,
+  Briefcase,
+  Users,
+  LifeBuoy,
+  Send,
+} from "lucide-react";
+import { NavSecondary } from "./nav-secondary";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "YC STARTUP",
+    email: "yc-startup@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Jobs",
+      url: "#",
+      icon: <Briefcase />,
+      isActive: true,
+      items: [
+        {
+          title: "Overview",
+          url: "#",
+        },
+        {
+          title: "Job Postings",
+          url: "#",
+        },
+        {
+          title: "Applications",
+          url: "#",
+        },
+        {
+          title: "Drafts",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Talent",
+      url: "#",
+      icon: <Users />,
+      items: [
+        {
+          title: "Talent Pool",
+          url: "#",
+        },
+        {
+          title: "Saved Talent",
+          url: "#",
+        },
+        {
+          title: "Outreach History",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: <Settings2Icon />,
+      items: [
+        {
+          title: "Company Profile",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "General",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+            <House />
+          </div>
+          <div className="grid flex-1 text-left text-lg leading-tight">
+            <span className="truncate font-medium">
+              <span className="font-bold">START</span>{" "}
+              <span className="font-light">Circle</span>
+            </span>
+            <span className="truncate text-xs">Dashboard</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
