3393beaa7bb61d2c618291324ff3cb76e1688d71
Jonathan Demme <j.b.demme@gmail.com>
2026-03-06 14:44:51 +0100

    add simple MVP talent Application form

---
+++ app/(onboarding)/application/startup/page.tsx
@@ -0,0 +1,3 @@
+export default function StartupApplicationPage() {
+  return <div>Startup Application Form</div>;
+}
--- app/(onboarding)/application/talent/page.tsx
+++ app/(onboarding)/application/talent/page.tsx
@@ -1,3 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { SPECIALITY_LABELS, SpecialitySchema } from "@/lib/schema";
+
 export default function TalentApplicationPage() {
-  return <div>Talent Application Form</div>;
+  return (
+    <div className="flex justify-center items-center p-4 md:p-8">
+      <TalentApplicationForm />
+    </div>
+  );
+}
+
+function TalentApplicationForm() {
+  return (
+    <form className="space-y-4 w-full">
+      <h1 className="font-bold tex2xl md:text-4xl">Talent Application Form</h1>
+      Full Name:
+      <Input placeholder="Sam Altman" />
+      Experience Level:
+      <Select>
+        <SelectTrigger>
+          <SelectValue placeholder="Select your experience level" />
+        </SelectTrigger>
+        <SelectContent position="popper">
+          {SpecialitySchema.options.map((option) => (
+            <SelectItem key={option} value={option}>
+              {SPECIALITY_LABELS[option]}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      Location:
+      <Input placeholder="eg. Vienna" />
+      LinkedIn URL:
+      <Input placeholder="https://linkedin.com/in/sam-altman" />
+      Contact Email:
+      <Input placeholder="contact@samaltman.com" />
+      Phone Number (optional):
+      <Input placeholder="+43 1 234 5678" />
+      <div className="flex justify-end gap-4">
+        <Button variant="outline">Cancel</Button>
+        <Button type="submit">Submit Application</Button>
+      </div>
+    </form>
+  );
 }
